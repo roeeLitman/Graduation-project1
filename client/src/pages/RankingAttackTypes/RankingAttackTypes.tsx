3d79c637c6c7d6e4bd83a03420bd4a54c5c5d4db
roee ltmanovich <roeeli770@gmail.com>
2024-12-22 13:47:01 +0200

    create base component for rank attack type

--- client/src/pages/RankingAttackTypes/RankingAttackTypes.tsx
+++ client/src/pages/RankingAttackTypes/RankingAttackTypes.tsx
@@ -7,55 +7,63 @@ import {
     LinearScale,
     Tooltip,
     Legend,
+    ArcElement,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { useAppDispatch, useAppSelector } from "../../redux/store";
-import { getAttack } from "../../redux/slices/attackSlice";
+import { Pie } from "react-chartjs-2";
 import { attackType } from "../../types/attackType";
-import { dataStatus } from "../../types/redux.typs";
+const colors = [
+  'rgba(255, 99, 132, 0.2)',  // אדום עם שקיפות 20%
+  'rgba(54, 162, 235, 0.2)',  // כחול עם שקיפות 20%
+  'rgba(255, 206, 86, 0.2)',  // צהוב עם שקיפות 20%
+  'rgba(75, 192, 192, 0.2)',  // ירוק עם שקיפות 20%
+  'rgba(153, 102, 255, 0.2)', // סגול עם שקיפות 20%
+  'rgba(255, 159, 64, 0.2)',  // כתום עם שקיפות 20%
+];
 
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
+ChartJS.register(ArcElement,BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 export default function RankingAttackTypes() {
-  const [dataFromApi, setData] = useState([]);
+    const [dataFromApi, setData] = useState([]);
 
-  useEffect(() => {
-    const getAttack = async () => {
-      const response = await fetch( `http://localhost:3000/api/typesAttack/get-rating`)
-      const data = await response.json()
-      setData(data)      
-    }
-    getAttack()
-  },[])
+    useEffect(() => {
+        const getAttack = async () => {
+            const response = await fetch(
+                `http://localhost:3000/api/typesAttack/get-rating/`
+            );
+            const data = await response.json();
+            setData(data);
+        };
+        getAttack();
+    }, []);
 
     const data = {
-        labels: dataFromApi.map((item: attackType) => item.name), 
+        labels: dataFromApi.map((item: attackType) => item.name),
         datasets: [
             {
-                label: "Sales", 
-                data: dataFromApi.map((item: attackType) => item.casualties), 
-                backgroundColor: "rgba(75, 192, 192, 0.2)",
-                borderColor: "rgba(75, 192, 192, 1)", 
-                borderWidth: 1, 
+                label: "Sales",
+                data: dataFromApi.map((item: attackType) => item.casualties),
+                backgroundColor: colors,
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1,
             },
         ],
     };
 
     const options = {
-        responsive: true, 
+        responsive: true,
         plugins: {
             legend: {
                 display: true,
             },
             tooltip: {
-                enabled: true, 
+                enabled: true,
             },
         },
     };
     return (
-    <div className="ranking-attack-types">
-    <Bar data={data} options={options} />
-    </div>
-    )
+        <div className="ranking-attack-types">
+            <Pie data={data} options={options} />
+        </div>
+    );
 }

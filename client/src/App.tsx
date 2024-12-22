b6d1eac949f13fdd640d8c4e3a7b538c2753e836
roee ltmanovich <roeeli770@gmail.com>
2024-12-22 10:19:27 +0200

    provide Braouzer router for main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-
+import RankingAttackTypes from "./pages/RankingAttackTypes/RankingAttackTypes";
 export default function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
-      </Routes>
-      </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<RankingAttackTypes/>} />
+            </Routes>
+        </div>
+    );
 }

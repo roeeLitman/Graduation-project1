dcb833b774d54b52b601b9562a0d93a72bf4526b
roee ltmanovich <roeeli770@gmail.com>
2024-12-22 09:31:25 +0200

    add 2 packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom";
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>}></Route>
+      </Routes>
+      </div>
   )
 }

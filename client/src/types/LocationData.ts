f97f7bd83a4155ff6c190e2133c81150ad069fcd
roee ltmanovich <roeeli770@gmail.com>
2024-12-23 10:42:39 +0200

    create iterfase for LocationData

---
+++ client/src/types/LocationData.ts
@@ -0,0 +1,8 @@
+export interface LocationData {
+    _id: string;       
+    city: string;      
+    casualties: number;
+    lat: number;       
+    long: number;      
+    __v: number;       
+}

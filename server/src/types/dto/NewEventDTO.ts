fe2ae2136a262fb38b3616eac14097c35a24e4fb
roee ltmanovich <roeeli770@gmail.com>
2024-12-24 11:35:41 +0200

    create interface

---
+++ server/src/types/dto/NewEventDTO.ts
@@ -0,0 +1,10 @@
+export interface NewEvent {
+    year: number;
+    month: number;
+    city: string
+    lat: number;
+    long: number;
+    attacktype: string
+    organization: string
+    casualties: number;
+  }

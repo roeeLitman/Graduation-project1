7d58a8f7b98b7abc7cc718d070c90941a295e99c
roee ltmanovich <roeeli770@gmail.com>
2024-12-24 12:06:29 +0200

    create function to create new attack

--- server/src/service/typeAttack.service.ts
+++ server/src/service/typeAttack.service.ts
@@ -1,14 +1,40 @@
 import TypesAttackModel from "../models/TypesAttack";
+import { NewEvent } from "../types/dto/NewEventDTO";
 
 export const listAttackSorted = async () => {
     try {
         return await TypesAttackModel.find()
-        .sort({ casualties: -1 })
-        .select('-listEvents')
-        .lean();
+            .sort({ casualties: -1 })
+            .select("-listEvents")
+            .lean();
     } catch (err) {
         console.error("[service]Error fetching types of attacks:", err);
-        throw err
+        throw err;
     }
 };
 
+export const createNewAttack = async (event: NewEvent) => {
+    try {
+        const attackModel = await TypesAttackModel.findOne({
+            name: event.attacktype,
+        });
+        if (!attackModel) {
+            const newTypesAttack = new TypesAttackModel({
+                name: event.attacktype,
+                casualties: event.casualties,
+                listEvents: [event._id],
+            });
+            await newTypesAttack.save();
+            return newTypesAttack;
+        } else {
+            await attackModel.updateOne({
+                $push: { listEvents: event._id },
+                $inc: { casualties: event.casualties },
+            });
+            return attackModel;
+        }
+    } catch (err) {
+        console.error("Error attack types:", err);
+        throw err;
+    }
+};

70a93d1a160cf1e3f360a51c7672473889b705dc
roee ltmanovich <roeeli770@gmail.com>
2024-12-24 13:43:14 +0200

    create new end point

--- server/src/controller/crad.controlier.ts
+++ server/src/controller/crad.controlier.ts
@@ -14,8 +14,8 @@ export const getAllEvents = async(req:Request<any,any,{page:number}>,res:Respons
 
 export const createNewEvent = async(req:Request<any,any,any, NewEvent>,res:Response) => {
     try {
-        const all = await createInformtionInAllColctions(req.body)
-        res.status(200).json(all)
+        const newEventFromDb = await createInformtionInAllColctions(req.body)
+        res.status(200).json(newEventFromDb)
     } catch (err) {
         res.status(400).json((err as Error).message)  
     }
--- server/src/routes/crud.router.ts
+++ server/src/routes/crud.router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllEvents } from "../controller/crad.controlier";
+import { createNewEvent, getAllEvents } from "../controller/crad.controlier";
 
 const cradRuter = Router()
 
 cradRuter.get("/get-hundred/:page",getAllEvents)
 
-cradRuter.post("/create-event",getAllEvents)
+cradRuter.post("/create-event", createNewEvent)
 
 
 export default cradRuter
